f05bb6099a6ac9bb03c168918f67521065afcbdb
Eric Galego <eric.galego@utoronto.mail.ca>
2018-11-01 22:10:37 -0400

    DailyScrumMeeting2 galegoer

--- Assignment2/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
+++ Assignment2/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
@@ -13,7 +13,7 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 
 		this.view = view;
 
-		String[] buttonLabels = { "Circle", "Rectangle", "Square", "Squiggle", "Polyline" };
+		String[] buttonLabels = { "Circle", "Rectangle", "Square", "Squiggle", "Polyline", "Fill", "Outline" };
 
 		int row = 0;
 		for (String label : buttonLabels) {
