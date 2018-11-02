9655e3815404c564b429d8c4a6528631fd42954e
liwill13 <willl.li@mail.utoronto.ca>
2018-11-01 21:20:55 -0400

    Bug1 in progress adding squiggle image -liwill13

--- Assignment2/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
+++ Assignment2/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
@@ -1,14 +1,20 @@
 package ca.utoronto.utm.paint;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-
+import javafx.scene.image.*;
+import java.io.FileInputStream; 
+import java.io.FileNotFoundException; 
 import javafx.scene.shape.Shape;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.SVGPath;
 
 public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEvent> {
 
@@ -19,26 +25,25 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 		this.view = view;
 		
 		//String[] buttonLabels = { "Circle", "Rectangle", "Square", "Squiggle", "Polyline" };
-	
 		//Point p = new Point(100,100);
+		
 		Circle c = new Circle();
 		//c.setCenterX(100.0f);
 		//c.setCenterY(100.0f);
 		c.setRadius(10.0f);
-		//Button button = new Button();
-		//button.setGraphic(c);
-		//button.setMinWidth(100);
-		//this.add(button, 0, 0);
 		
 		Rectangle r = new Rectangle();
 		r.setHeight(20.0f);
-		r.setWidth(20.0f);
-		//Button button2 = new Button();
-		//button2.setGraphic(r);
-		//button2.setMinWidth(100);
-		//this.add(button2, 0, 0);
+		r.setWidth(35.0f);
 		
-		Shape[] shapes = {c, r};
+		Rectangle t = new Rectangle();
+		t.setHeight(20.0f);
+		t.setWidth(20.0f);
+		
+		SVGPath svg = new SVGPath();
+		
+		
+		Shape[] shapes = {c, r, t};
 		
 			
 		int row = 0;
@@ -50,6 +55,18 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 			row++;
 			button.setOnAction(this);
 		}
+		
+		System.out.println(new File(".").getAbsolutePath());
+		FileInputStream inputstream = new FileInputStream("C:\\Users\\William\\repo_a2_TheCentipedeBoys\\Assignment2\\images\\squiggle.png");
+		//Image image = new Image("images/squiggle.png", 50, 50, true, true);
+		//ImageView i = new ImageView(image);
+		//i.setX(100);
+		//i.setY(100);
+
+		//Button button = new Button();
+//		button.setGraphic(new ImageView(image));
+//		button.setMinWidth(100);
+//		this.add(button, 0, row);
 	}
 
 	@Override
