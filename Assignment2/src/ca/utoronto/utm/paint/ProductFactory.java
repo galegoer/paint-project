b566a53e75a258bef821f518f2306d54aba7daa5
Eric Galego <eric.galego@utoronto.mail.ca>
2018-11-07 23:57:05 -0500

    Rough outline Design Pattern for repaint

---
+++ Assignment2/src/ca/utoronto/utm/paint/ProductFactory.java
@@ -0,0 +1,20 @@
+package ca.utoronto.utm.paint;
+
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+public class ProductFactory {
+//	private HashMap<ID, Class> RegisteredShapes = new HashMap<ID, Class>();
+//	public void registerProduct (String productID, Class productClass) {
+//		RegisteredShapes.put(productID, productClass)
+//	}
+//	
+//	public Product createProduct(String productID) {
+//		Class productClass = (Class)RegisteredShapes.get(productID);
+//		Constructor productConstructor = Class.getDeclaredConstructor(new Class[] { String.class });
+//		return (Product)productConstructor.newInstance(new Object[] { });
+//	}
+}
