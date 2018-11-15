85260d7b3bf75a44cf57e6462624053cdebd9422
liwill13 <willl.li@mail.utoronto.ca>
2018-11-14 21:58:55 -0500

    Bug 10 started

---
+++ Assignment2/src/ManipulatorStrategies/RoundedRectangleManipulatorStrategy.java
@@ -0,0 +1,19 @@
+package ManipulatorStrategies;
+
+import javafx.scene.input.MouseEvent;
+
+public class RoundedRectangleManipulatorStrategy implements ShapeManipulatorStrategies {
+
+	@Override
+	public void createShape() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shapeBehaviour(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Assignment2/src/ManipulatorStrategies/ShapeManipulatorStrategies.java
@@ -0,0 +1,8 @@
+package ManipulatorStrategies;
+
+import javafx.scene.input.MouseEvent;
+
+public interface ShapeManipulatorStrategies {
+	public void createShape();
+	public void shapeBehaviour(MouseEvent e);
+}
